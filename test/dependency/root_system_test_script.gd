56b8823ee25de0536f1b697873d37fb76550932c
mayank <mayankprasoontirkey56@gmail.com>
2021-12-15 14:23:20 +0530

    root save file clean up scripted updated

--- test/dependency/root_system_test_script.gd
+++ test/dependency/root_system_test_script.gd
@@ -16,23 +16,22 @@ func clean_system_save_files():
 	var base_folder:String = "res://save_files/"
 	
 	for x in sub_folders:
-		base_folder = base_folder + '/' + x
-		if folder.dir_exists(base_folder):
-			var _x = folder.open(base_folder)
+		var new_folder = base_folder + x
+		if folder.dir_exists(new_folder):
+			var _x = folder.open(new_folder)
 			var _y = folder.list_dir_begin()
 			while true:
 				var file = folder.get_next()
 				if file == "":
 					break
 				elif not file.begins_with("."):
-					var _z = folder.remove(base_folder + '/' + file)
-					gut.p("[color=#e51e48][Removed]: {file}[/color]".format({"file" : (base_folder + '/' + file)}))
+					var _z = folder.remove(new_folder + '/' + file)
+					gut.p("[color=#e51e48][Removed]: {file}[/color]".format({"file" : (new_folder + '/' + file)}))
 			folder.list_dir_end()
 
 func before_all():
 	gut.p("\n=>\t[color=#e6d11d]### SYSTEM TESTS ###[/color]")
 	clean_system_save_files()
-	
 
 func after_each():
 	clean_system_save_files()
