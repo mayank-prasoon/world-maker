0ae509b485c1907d70c0fee658d174fb697d0b12
Mayank Prasoon <mayankprasoontirkey56@gmail.com>
2021-12-22 02:19:53 +0530

    new setup

---
+++ singletone/command_system.gd
@@ -0,0 +1,16 @@
+extends Node
+
+func _ready():
+	pass # Replace with function body.
+
+
+func _on_CommandInput_text_entered(new_text):
+	pass # Replace with function body.
+
+
+func _on_CommandInput_text_changed(new_text):
+	pass # Replace with function body.
+
+
+func _on_RunButton_pressed():
+	pass # Replace with function body.
---
+++ singletone/command_system_dependecy/command_list.gd
@@ -0,0 +1,3 @@
+extends Node
+
+enum COMMANDS_TYPE
