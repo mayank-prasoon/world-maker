54fb7bea22fbd31caaf397ec8a726bd640f51a2f
Mayank Prasoon <mayankprasoontirkey56@gmail.com>
2021-12-24 17:12:09 +0530

    map system updated with logging and echo

--- singletone/system_data_manager_dependency/chunk_system/map_chunk_save_system.gd
+++ singletone/system_data_manager_dependency/chunk_system/map_chunk_save_system.gd
@@ -1,22 +1,26 @@
 extends SaveSystem
 
 func _ready():
+	LoggingSystem.log_new_event(name + " - node loaded")
+
 	save_location = self.root_node.root_save_file_path + '/map_chunks/{uuid}_save_data.tres'
 	resource_type = self.root_node.MAP_RESOURCE_TYPE.MAP_CHUNK
 	
 func save_file(
 		chunk_name:String,
 		chunk_id:String = root_node.uuid_util.v4(),
-		texture:String  = "xyz",
-		offset:Vector2  = Vector2(0,0),
+		chunk_texture:String  = "xyz",
+		chunk_offset:Vector2  = Vector2(0,0),
 		pins:Array      = []
 	) -> void:
+
+	LoggingSystem.log_new_event(name + " - " + "save_file({0},{1},{2},{3},{5})".format([chunk_name, chunk_id, chunk_texture, chunk_offset, str(pins)]))
 
 	var newMapChunkData               = root_node.mapChunk.new()
 	newMapChunkData.chunk_name        = chunk_name
 	newMapChunkData.chunk_id          = chunk_id
-	newMapChunkData.chunk_offset      = offset
-	newMapChunkData.chunk_texture     = texture
+	newMapChunkData.chunk_offset      = chunk_offset
+	newMapChunkData.chunk_texture     = chunk_texture
 	newMapChunkData.chunk_pins        = pins
 
 	var _x = ResourceSaver.save(
--- singletone/system_data_manager_dependency/chunk_system/map_chunk_system.gd
+++ singletone/system_data_manager_dependency/chunk_system/map_chunk_system.gd
@@ -2,3 +2,6 @@ extends Node
 
 onready var save_system    = get_node("SaveSystem")
 onready var system_manager = get_node("MapChunkManager")
+
+func _ready():
+    LoggingSystem.log_new_event(name + " - loaded")
