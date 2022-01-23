221d9e13631029d29d25f074c6f00ab9bb221fca
Mayank Prasoon <mayankprasoontirkey56@gmail.com>
2022-01-23 17:59:15 +0530

    added template property to root article resource template

--- assets/file templates/save_system_template/root_article_resources_template.gd
+++ assets/file templates/save_system_template/root_article_resources_template.gd
@@ -4,7 +4,7 @@ extends Resource
 export(String)         var article_name:String
 export(String)         var article_id:String
 export(String, FILE)   var article_profile:String
+export(int)            var article_type:int
 export(String, FILE)   var article_banner:String
-export(Resource)       var article_prompts:Resource
-export(String)         var article_raw_data:String
+export(Resource)       var article_template:Resource
 export(Array, String)  var tags:Array
