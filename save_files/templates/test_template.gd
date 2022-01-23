947d62df0aa664be2348302c0831f638eb35b398
Mayank Prasoon <mayankprasoontirkey56@gmail.com>
2022-01-23 20:21:02 +0530

    Revert "Merge branch 'wiki-dashboard-UI' of github.com:mayank-prasoon/world-building-software into wiki-dashboard-UI"
    This reverts commit d738b876eb0989dcbecf87f912f5cc29f272dfd1, reversing
    changes made to 307b3cdcda9b072dd24123d4d6af3f6904f21cd5.

---
+++ save_files/templates/test_template.gd
@@ -0,0 +1,6 @@
+extends Resource
+
+export(String)            var template_name:String   = "test_name"
+export(int)               var template_type:int      = 0
+export(Array, Dictionary) var template_prompts:Array = []
+export(Array, Dictionary) var display:Array          = []
