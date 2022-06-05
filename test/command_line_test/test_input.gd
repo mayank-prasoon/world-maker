07b36b606a83be12f32cafdf369ec3e8edf5d035
mayank <mayankprasoontirkey56@gmail.com>
2022-06-05 21:34:56 +0530

    removed test

--- test/command_line_test/test_input.gd
+++
@@ -1,5 +0,0 @@
-extends Node
-
-
-func _init():
-	ProjectSettingsManager.shortcut_settings.remap_input("command line interface", load("res://test/resources_and_temp_items/command_line_shortcut.tres"))
